2a9d9f97f8c7f19cb76307839e86a55f4e33d94a
Leon <Leon@Asus>
2017-03-21 16:30:03 -0500

    Laboratorio uno

---
+++ Conta.X/build/default/production/conversor.o.d
@@ -0,0 +1,5 @@
+build/default/production/conversor.o:  \
+	C:/Users/Camilo/Documents/Documents/Camilo/MICROCONTROLADORES/I_2017/ContadorLAB1/Conta.X/conversor.asm  \
+	C:/Users/Camilo/Documents/Documents/Camilo/MICROCONTROLADORES/I_2017/ContadorLAB1/Conta.X/p16f887.inc  \
+	C:/Users/Camilo/Documents/Documents/Camilo/MICROCONTROLADORES/I_2017/ContadorLAB1/Conta.X/tempo.inc  \
+
